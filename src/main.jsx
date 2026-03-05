b91265c797f08ed68144ea60e769eb609e37626f
thomas-jichen <tjwang@stanford.edu>
2026-03-04 22:03:04 -0500

    vercel fix 2 ps. evan is mad zooted

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App'
 import LandingLayout from './LandingLayout'
 import LandingPage from './pages/LandingPage'
@@ -34,5 +35,6 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
+    <Analytics />
   </StrictMode>
 )
